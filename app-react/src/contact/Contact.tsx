65f6142f2d7bec6ebe1e4f77103d55f43c10a752
omar-su <73144494+Omar-su@users.noreply.github.com>
2023-03-19 20:06:25 +0100

    Contact page css and contact form

--- app-react/src/contact/Contact.tsx
+++ app-react/src/contact/Contact.tsx
@@ -1,6 +1,11 @@
 import { NavBar } from "../utils/NavigationBar";
 import "./Contact.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ReCAPTCHA from 'react-google-recaptcha';
+import { useState } from "react";
+import axios from 'axios';
+
+const SITEKEY_ENV = process.env.REACT_APP_RECAPTCHA_TOKEN;
 
 export function ContactPage(){
 
@@ -25,8 +30,58 @@ export function ContactPage(){
 
 function ContactForm(){
 
+  const [isHuman, setIsHuman] = useState(false);
+
+  function handleSubmit(e: { preventDefault: () => void; }) {
+    e.preventDefault();
+    if(isHuman){
+      const name = (document.getElementById("inputName") as HTMLInputElement).value;
+      const lastName = (document.getElementById("inputLastName") as HTMLInputElement).value;
+      const email = (document.getElementById("inputEmail") as HTMLInputElement).value;
+      const subject = (document.getElementById("inputSubject") as HTMLInputElement).value;
+      const message = (document.getElementById("inputMessage") as HTMLTextAreaElement).value;
+
+
+      console.log("submitted ");
+      console.log(`Name: ${name}\nLast Name: ${lastName}\nEmail: ${email}\nSubject: ${subject}\nMessage: ${message}`);
+
+    } else {
+      // show error message or redirect to CAPTCHA
+      console.log("not submitted");
+    }
+  }
+  
+  // function verifyToken(token: string) {
+  //   axios.post('/api/verify-recaptcha', { token }, {
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     }
+  //   })
+  //   .then((response: { data: any; }) => {
+  //     const data = response.data;
+  //     if (data.success) {
+  //       setIsHuman(true);
+  //     } else {
+  //       setIsHuman(false);
+  //     }
+  //   })
+  //   .catch((error: any) => {
+  //     console.error('Error verifying reCAPTCHA token:', error);
+  //   });
+  // }
+
+  function onChange(value: any) {
+    // send the reCAPTCHA token to the server for verification
+    // verifyToken(value);
+    setIsHuman(true);
+  }
+
+  function onExpired() {
+    setIsHuman(false);
+  }
+
   return(
-      <form>
+      <form onSubmit={handleSubmit}>
       <div className="form-row">
         <div className="form-group col-md-6">
           <label htmlFor="inputEmail4">Name*</label>
@@ -47,10 +102,13 @@ function ContactForm(){
       </div>
       <div className="form-group message-form">
         <label htmlFor="inputSubject">Message *</label>
-        <textarea className="form-control" id="inputSubject" placeholder=""></textarea>
+        <textarea className="form-control" id="inputMessage" placeholder=""></textarea>
       </div>
-
-    <button type="submit" className="btn">Submit</button>
+      {SITEKEY_ENV ? (<div className="recaptcha">
+        <ReCAPTCHA sitekey={SITEKEY_ENV} onChange={onChange} onExpired={onExpired}/>
+      </div>) : 
+      (<div>Loading...</div>)}
+      <button type="submit" className="btn" disabled={!isHuman}>Submit</button>
   </form>
   );
 }
