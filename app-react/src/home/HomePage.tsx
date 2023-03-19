028367bac6078a6db4eae16528beec6d4f501455
omar-su <73144494+Omar-su@users.noreply.github.com>
2023-03-19 20:06:39 +0100

    Home page css and html

--- app-react/src/home/HomePage.tsx
+++ app-react/src/home/HomePage.tsx
@@ -1,6 +1,9 @@
 import { NavBar } from "../utils/NavigationBar";
 import "./Home.css"
 import portfolioMain from '../images/portfolioMain.jpg';
+import 'boxicons/css/boxicons.min.css';
+const Typical = require('react-typical').default;
+
 
 export function HomePage(){
 
@@ -8,13 +11,32 @@ export function HomePage(){
     <div>
       <NavBar></NavBar>
     </div>
-    <div className="main-info">
-      <div>
-        <h1>BRIEF BIO</h1>
-        <p>Software engineer at Chalmers </p> 
-      </div>     
-      <img src={portfolioMain} alt="yuuu" />
-    </div>
+    <section className="main-info">
+      <div className="info">
+        <h3>Hello, It's Me</h3>
+        <h1>Omar Sulaiman</h1>
+        <h3>And I am a <span><Typical
+          steps={['Software engineer', 1000, 'Fullstack Developer', 1000,'Student at Chalmers', 1000,'Youtuber ich :)', 1000]}
+          loop={Infinity}
+          wrapper="span"/></span>
+      </h3>
+        <div className="social-media">
+          <a href="#"><i className='bx bxl-facebook'></i></a>
+          <a href="#"><i className='bx bxl-instagram' ></i></a>
+          <a href="#"><i className='bx bxl-youtube' ></i></a>
+          <a href="#"><i className='bx bxl-linkedin' ></i></a>  
+        </div>   
+        <a href="#" className="btn-cv">Download CV</a>  
+      </div> 
+
+      <div className="my-image">
+        <img src={portfolioMain} alt="yuuu" />
+      </div>
+    </section>
+
+    
+    
+
   </div>);
 
 } 
