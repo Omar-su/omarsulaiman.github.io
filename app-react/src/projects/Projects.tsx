4ac0a9469c8d77399ffb5dc6775ba3f5574028f9
omar-su <73144494+Omar-su@users.noreply.github.com>
2023-03-19 20:06:57 +0100

    Template for projects

--- app-react/src/projects/Projects.tsx
+++ app-react/src/projects/Projects.tsx
@@ -1,15 +1,65 @@
 import { NavBar } from "../utils/NavigationBar";
 import "./Projects.css"
+import portfolioMain from '../images/portfolioMain.jpg';
 
 export function ProjectsPage(){
 
-  return(<div>
+  return( 
     <div>
-      <NavBar></NavBar>
+      <div>
+        <NavBar></NavBar>
+      </div>
+      {/* <div className="text">
+        Welcome to projects
+      </div> */}
+      {/* <iframe frameBorder="0" src="https://itch.io/embed/1976848" width="552" height="167"><a href="https://omarsu.itch.io/godamongus">GodAmongUs by omarsu</a></iframe> */}
+      <section className="portfolio" >
+        <h2 className="heading">Latest <span>Projects</span></h2>
+        
+        <div className="portfolio-container">
+          <div className="portfolio-box">
+            <img src={portfolioMain} alt=""></img>
+            <div className="portfolio-layer">
+              <h4>Unity 2D Game</h4>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, ratione.</p>
+              <a href="#"><i className='bx bx-link-external'></i></a>
+            </div>
+          </div>
+
+          <div className="portfolio-box">
+            <img src={portfolioMain} alt=""></img>
+            <div className="portfolio-layer">
+              <h4>IMat</h4>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, ratione.</p>
+              <a href="#"><i className='bx bx-link-external'></i></a>
+            </div>
+          </div>
+
+          <div className="portfolio-box">
+            <img src={portfolioMain} alt=""></img>
+            <div className="portfolio-layer">
+              <h4>Ratar</h4>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, ratione.</p>
+              <a href="#"><i className='bx bx-link-external'></i></a>
+            </div>
+          </div>
+
+          <div className="portfolio-box">
+            <img src={portfolioMain} alt=""></img>
+            <div className="portfolio-layer">
+              <h4>ReTwitter</h4>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, ratione.</p>
+              <a href="#"><i className='bx bx-link-external'></i></a>
+            </div>
+          </div>
+          
+        </div>
+
+
+        
+      </section>
+
     </div>
-    <div className="text">
-      Welcome to projects
-    </div>
-  </div>);
+  );
 
 }
