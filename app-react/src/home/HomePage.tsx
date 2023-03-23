b83d13d3fabc618c5bebf25819b603c9487f5b52
omar-su <73144494+Omar-su@users.noreply.github.com>
2023-03-24 00:16:16 +0100

    Connecting links to icons

--- app-react/src/home/HomePage.tsx
+++ app-react/src/home/HomePage.tsx
@@ -4,7 +4,6 @@ import portfolioMain from '../images/portfolioMain.jpg';
 import 'boxicons/css/boxicons.min.css';
 const Typical = require('react-typical').default;
 
-
 export function HomePage(){
 
   return(<div>
@@ -21,12 +20,12 @@ export function HomePage(){
           wrapper="span"/></span>
       </h3>
         <div className="social-media">
-          <a href="#"><i className='bx bxl-facebook'></i></a>
-          <a href="#"><i className='bx bxl-instagram' ></i></a>
-          <a href="#"><i className='bx bxl-youtube' ></i></a>
-          <a href="#"><i className='bx bxl-linkedin' ></i></a>  
+          <a href="https://www.facebook.com/profile.php?id=100009854089080" target="_blank"><i className='bx bxl-facebook'></i></a>
+          <a href="https://www.instagram.com/omarsuliman10/" target="_blank"><i className='bx bxl-instagram' ></i></a>
+          <a href="https://www.youtube.com/@omars4768" target="_blank"><i className='bx bxl-youtube' ></i></a>
+          <a href="https://www.linkedin.com/in/omar-sulaiman-3b509121b/" target="_blank"><i className='bx bxl-linkedin' ></i></a>  
         </div>   
-        <a href="#" className="btn-cv">Download CV</a>  
+        <a href='#' download className="btn-cv">Download CV</a>  
       </div> 
 
       <div className="my-image">
