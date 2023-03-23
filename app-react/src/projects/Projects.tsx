d8738ecace6c2d0c4033cc73a6557e0ca394c47d
omar-su <73144494+Omar-su@users.noreply.github.com>
2023-03-24 00:17:01 +0100

    Refactoring project page

--- app-react/src/projects/Projects.tsx
+++ app-react/src/projects/Projects.tsx
@@ -1,9 +1,6 @@
 import { NavBar } from "../utils/NavigationBar";
 import "./Projects.css"
-import reTwitterImg from '../images/reTwitter.jpg';
-import imat from '../images/imat.jpg';
-import leefoAll from '../images/leefoAll.jpg';
-import figmaMap from '../images/figmaMap.jpg';
+import { projectsData } from "./ProjectsData";
 
 export function ProjectsPage(){
 
@@ -12,57 +9,41 @@ export function ProjectsPage(){
       <div>
         <NavBar></NavBar>
       </div>
-      {/* <div className="text">
-        Welcome to projects
-      </div> */}
-      {/* <iframe frameBorder="0" src="https://itch.io/embed/1976848" width="552" height="167"><a href="https://omarsu.itch.io/godamongus">GodAmongUs by omarsu</a></iframe> */}
       <section className="portfolio" >
         <h2 className="heading">Latest <span>Projects</span></h2>
         
         <div className="portfolio-container">
-          <div className="portfolio-box">
-            <img src={reTwitterImg} alt=""></img>
-            <div className="portfolio-layer">
-              <h4>ReTwitter</h4>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, ratione.</p>
-              <a href="#"><i className='bx bx-link-external'></i></a>
-            </div>
-          </div>
 
-          <div className="portfolio-box">
-            <img src={figmaMap} alt=""></img>
-            <div className="portfolio-layer">
-              <h4>SoldierE4</h4>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, ratione.</p>
-              <a href="#"><i className='bx bx-link-external'></i></a>
-            </div>
-          </div>
-
-          <div className="portfolio-box">
-            <img src={leefoAll} alt=""></img>
-            <div className="portfolio-layer">
-              <h4>Leefo</h4>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, ratione.</p>
-              <a href="#"><i className='bx bx-link-external'></i></a>
-            </div>
-          </div>
-
-          <div className="portfolio-box">
-            <img src={imat} alt=""></img>
-            <div className="portfolio-layer">
-              <h4>IMat</h4>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, ratione.</p>
-              <a href="#"><i className='bx bx-link-external'></i></a>
-            </div>
-          </div>
+            {projectsData.map((project) => (
+              <ProjectCard src={project.image} name={project.name} desc={project.desc} link={project.link}></ProjectCard>
+            ))}
           
         </div>
-
-
         
       </section>
 
     </div>
   );
 
+}
+
+interface ProjectProps {
+  src : string,
+  name : string,
+  desc : string,
+  link : string
+}
+
+function ProjectCard({src, name, desc, link} : ProjectProps ) {
+
+  return(
+    <div className="portfolio-box">
+    <img src={src} alt=""></img>
+    <div className="portfolio-layer">
+      <h4>{name}</h4>
+      <p>{desc}</p>
+      <a href={link} target="_blank"><i className='bx bx-link-external'></i></a>
+    </div>
+  </div>
+  );
 }
