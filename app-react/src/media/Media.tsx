1f20124c1da15dc636a70b544b7decfd50bd49c5
omar-su <73144494+Omar-su@users.noreply.github.com>
2023-03-24 00:16:36 +0100

    Sound

--- app-react/src/media/Media.tsx
+++ app-react/src/media/Media.tsx
@@ -1,7 +1,20 @@
+import { useRef, useState } from "react";
 import { NavBar } from "../utils/NavigationBar";
-import "./Media.css"
+import "./Media.css";
+const dragonIntro = require("../assets/dragonIntro.wav");
 
 export function MediaPage(){
+
+
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  function playSound () {
+    if (!isPlaying) {
+      new Audio(dragonIntro).play();      
+      setIsPlaying(true);
+    }
+  };
 
   return(<div>
     <div>
@@ -11,6 +24,7 @@ export function MediaPage(){
       Welcome to Media
     </div>
     <iframe className="video-player" width="560" height="315" src="https://www.youtube.com/embed/1HWPRu7tea4" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+    <button onClick={playSound}>{isPlaying ? 'Pause Music' : 'Play Music'}</button>
 
   </div>);
 
